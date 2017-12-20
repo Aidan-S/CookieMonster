ba72b7c91bf7a1b18ac5730922469adcbbcc612b
scannella <scannella@bishopireton.org>
2017-12-20 11:01:18 -0500

    moreFun

--- src/CookieMonster.java
+++ src/CookieMonster.java
@@ -2,8 +2,8 @@ import java.util.Stack;
 
 public class CookieMonster {
 	
-	Stack<Path> Cookies;
-	int Total;
+	Stack<Path> tower;
+	int soFar;
 	int Max;
 	int[][] cookies = new int[][]{
 		  {1,  3,  0,  5, -1,  7, -1, -1,  0,  4,  2,  1},
@@ -20,11 +20,24 @@ public class CookieMonster {
 		  {0 , 0 , 3,  1,  5 , 2 , 1 , 5 , 4 , 1 , 3,  3},
 		};
 	int size = cookies[0].length + cookies.length - 1;
-		Path p = new Path(0, 0, size);
+	Path p = new Path(0, 0, size, 0);
 		
 		
 	CookieMonster(){
-		
+		int s = 0;
+		while(s < size) {
+			soFar += cookies[p.getX()][p.getY()];
+			
+			if(cookies[p.getX()][p.getY()+1] != -1) {
+				p.setX(p.getX()+1);
+			}
+			
+			if(cookies[p.getX()+1][p.getY()] != -1) {
+				Path t = p;
+				tower.push(t);
+			}
+			
+		}
 	}
 		
 	
--- src/Path.java
+++ src/Path.java
@@ -1,7 +1,39 @@
 
 public class Path {
 
-	Path(int a, int b, int c){
-		
+	int x;
+	int y;
+	int l;
+	int s;
+	
+	Path(int a, int b, int c, int size){
+		x = a;
+		y = b;
+		l = c;
+		s = size;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public int getL() {
+		return l;
+	}
+	
+	public void setX(int b) {
+		x = b;
+	}
+	
+	public void setY(int b) {
+		y = b;
+	}
+	
+	public void setL(int b) {
+		l = b;
 	}
 }
